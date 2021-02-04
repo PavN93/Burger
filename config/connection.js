248d12ef25a2b0df30d4fe58601c292d16723f14
Pawel <paweln993@gmail.com>
2021-02-04 18:56:34 +0000

    manifest for pwa added

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 const mysql = require("mysql");
 require('dotenv').config();
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
 // Establish connection
 connection.connect(function (err) {
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 var exphbs = require("express-handlebars");
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.SV_PORT || 3030;
 
 app.use(express.json());
 app.use(express.static("public"));
